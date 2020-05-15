0f49bea061dfa885449b5ce39e606726ba05da4a
ZorquanOne <alexparshinwork@gmail.com>
2020-05-15 13:36:54 +0300

    Login

--- case/urls.py
+++ case/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from . import views
-
+from django.urls import include
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('regestr/', views.regestr, name='regestr'),
-    
 ]
