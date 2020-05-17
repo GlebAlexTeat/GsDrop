db9b1a0393f5b75da060e81ae31c05f75e3ca4a2
ZorquanOne <alexparshinwork@gmail.com>
2020-05-17 21:15:36 +0300

    ''

--- case/models.py
+++ case/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Profile(models.Model):
 
     user = models.OneToOneField( settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING )
-    Money = models.DecimalField(max_digits=6, decimal_places=2)
-    Icon = models.CharField(max_length=100, help_text="Enter field documentation", null=True)
-    Rang = models.DecimalField(max_digits=3, decimal_places=2,  null=True)
-    Cance = models.DecimalField(max_digits=2, decimal_places=2,  null=True)
-    User_Gun = models.TextField(help_text='Enter gun piy piy', null=True)
+    Money = models.DecimalField(max_digits=6, decimal_places=2,default=0)
+    Icon = models.CharField(max_length=100, help_text="Enter field documentation", null=True,default=0)
+    Rang = models.DecimalField(max_digits=3, decimal_places=2,  null=True,default=0)
+    Cance = models.DecimalField(max_digits=2, decimal_places=2,  null=True,default=0.98)
+    User_Gun = models.TextField(help_text='Enter gun piy piy', null=True,default='')
     class Meta: 
         ordering = ["-user"]
 
@@ -19,6 +21,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return 'Profile for user {}'.format(self.user.username)
+
+    @receiver(post_save, sender=User) 
+    def new_user(sender, instance, created, **kwargs): 
+     if created: 
+        Profile.objects.create(user=instance) 
+        instance.profile.save()
 
 class Gun(models.Model):
     title_gun = models.CharField(max_length=40, help_text="Enter field documentation")
